f6ab72d9022fc4f1c5eb3cfd9a40cd28e1513a15
Nick Valdez <nicholastvaldez@gmail.com>
2023-06-30 11:54:30 -0500

    add logic to refresh spotify access token

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,30 @@
-import { LOGIN_URL } from "@/lib/spotify"
+import spotifyApi, { LOGIN_URL } from "@/lib/spotify"
 import NextAuth from "next-auth"
 import SpoitifyProvider from "next-auth/providers/spoitify"
+
+async function refreshAccessToken(token) {
+  try {
+    spotifyApi.setAccessToken(token.accessToken)
+    spotifyApi.setRefreshToken(token.refreshToken)
+
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken()
+    console.log("REFRESHED TOKEN IS", refreshedToken)
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now + refreshedToken.expires_in * 1000,
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+    }
+  } catch (error) {
+    console.error(error)
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    }
+  }
+}
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -11,6 +35,33 @@ export const authOptions = {
       authorization: LOGIN_URL,
     }),
   ],
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async jwt({ token, account, user }) {
+      // Initial sign in
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpires: account.expires_at * 1000, //expiry times in ms
+        }
+      }
+      // Return previous token if the access token has not expired yet
+      if (Date.now() < token.accessTokenExpires) {
+        console.log("EXISTING ACCESS TOKEN IS VALID")
+        return token
+      }
+
+      // Access token has expired. Refresh it
+      console.log("ACCESS TOKEN HAS EXPIRED, REFRESHING...")
+      return await refreshAccessToken(token)
+    },
+  },
 }
 
 export default NextAuth(authOptions)
