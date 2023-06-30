b560052fe5a83ffb72550d404099c33a8f5bc048
Nick Valdez <nicholastvaldez@gmail.com>
2023-06-30 12:00:46 -0500

    added session object to next auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -61,6 +61,14 @@ export const authOptions = {
       console.log("ACCESS TOKEN HAS EXPIRED, REFRESHING...")
       return await refreshAccessToken(token)
     },
+
+    async session({ session, token }) {
+      session.user.accessToken = token.accessToken
+      session.user.refreshToken = token.refreshToken
+      session.user.usename = token.username
+
+      return session
+    },
   },
 }
 
